3a8e4495a2ff271c4e8a868df1d4966f4df0e499
edouardo <edouardoaf2@gmail.com>
2025-10-09 13:07:54 +0100

    redux toolkit added and slices examples

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext'
+import { store } from './store/store.js'
+import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
+    <Provider store={store}>
       <App />
-    </AuthProvider>
-    
+    </Provider>
   </StrictMode>,
 )
