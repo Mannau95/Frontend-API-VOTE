6490340ff57cdfd19192cbb552a3d6a05e59c3f5
edouardo <edouardoaf2@gmail.com>
2025-10-15 15:59:53 +0100

    strict mode off

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,8 @@ import { store } from './store/store.js'
 import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+
     <Provider store={store}>
       <App />
-    </Provider>
-  </StrictMode>,
+    </Provider>,
 )
