077f7ae67d315e318a1a7104ff80f1589342223c
edouardo Afangnon <edouardo.afangnon@uptimise.io>
2025-11-28 19:25:21 +0100

    feat: add store provider to app

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext'
+import { Provider } from 'react-redux';
+import {store} from "./store/store.js";
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>,
+    <Provider store={store}>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+    </Provider>,
     
   // </StrictMode>,
 )
