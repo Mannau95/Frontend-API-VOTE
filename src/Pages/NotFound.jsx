da1fe3fbd43ce31644d72fc2d2b58df04b3fa9bc
edouardo Afangnon <edouardo.afangnon@uptimise.io>
2025-11-29 09:00:27 +0100

    feat: add not found route and page

---
+++ src/Pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NotFound() {
+    return (
+        <div className="h-[93dvh] flex">
+            <p className="m-auto">Cette page n'existe pas. Revenir au menu?</p>
+        </div>
+    );
+}
+
+export default NotFound;
